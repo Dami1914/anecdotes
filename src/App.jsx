44e97d0d6f72eda9bf949c9c9f8ae890659c22f1
Damilola Jibowu <Aesthetico.design@gmail.com>
2023-09-10 11:34:11 +0100

    part1 1.13* anecdotes added vote to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,52 @@
-import { useState } from 'react'
-
+import { useState } from "react";
 
 function App() {
-
   const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-  const [currentAnec,setCurrentAnec] = useState("anecdotes displays here")
-  const [vote, setVote] = useState(Array(anecdotes.length))
-  
-  function SwitchAnecdotes(){
-    setCurrentAnec(anecdotes[Math.floor(Math.random()*anecdotes.length)])
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  const [currentAnec, setCurrentAnec] = useState("anecdotes displays here");
+  const [vote, setVote] = useState(Array(anecdotes.length).fill(0));
+
+  function SwitchAnecdotes() {
+    setCurrentAnec(anecdotes[Math.floor(Math.random() * anecdotes.length)]);
+  }
+
+  function IncVotes() {
+    setVote((prev) =>
+      prev.map((ele, i) =>
+        i === anecdotes.indexOf(currentAnec) ? ele + 1 : ele
+      )
+    );
+    console.log(vote);
   }
 
   return (
     <>
       <div>{currentAnec}</div>
-      <button onClick={SwitchAnecdotes}>anecdotes</button>
+      <div>
+        {!anecdotes.includes(currentAnec)
+          ? ""
+          : vote[anecdotes.indexOf(currentAnec)] < 2
+          ? `has ${vote[anecdotes.indexOf(currentAnec)]} vote`
+          : `has ${vote[anecdotes.indexOf(currentAnec)]} votes`}
+      </div>
+      <div>
+        {!anecdotes.includes(currentAnec) ? (
+          ""
+        ) : (
+          <button onClick={IncVotes}>vote</button>
+        )}
+        <button onClick={SwitchAnecdotes}>next anecdotes</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
